6a774d57ecf85a37bde174b3e9eeb5ff33317194
HyperSpace159 <ideology888@gmail.com>
2025-12-30 14:29:46 +0700

    fix: force androidx.core 1.13.1 and downgrade appcompat to 1.7.0 to resolve API 35 requirement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,13 @@ plugins {
     id("com.jaredsburrows.license")
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("androidx.core:core:1.13.1")
+        force("androidx.core:core-ktx:1.13.1")
+    }
+}
+
 android {
     namespace = "com.v2ray.ang"
     compileSdk = 34
